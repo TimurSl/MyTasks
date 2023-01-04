2c12ff766c77dc883bb1d4efd238f388f9b1d6fe
Zenisoft <60827259+TimurSl@users.noreply.github.com>
2023-01-04 14:52:24 +0200

    Solved another 10 tasks in Boolean
    Total tasks solved: 90

--- Tasks/Boolean.cs
+++ Tasks/Boolean.cs
@@ -14,6 +14,16 @@ public class Boolean
         Console.WriteLine(Boolean8(5, 63));
         Console.WriteLine(Boolean9(5, 63));
         Console.WriteLine(Boolean10(5, 63));
+        Console.WriteLine(Boolean11(5, 63));
+        Console.WriteLine(Boolean12(5, 123, 1));
+        Console.WriteLine(Boolean13(5, 123, 45));
+        Console.WriteLine(Boolean14(5, 187, 45));
+        Console.WriteLine(Boolean15(5, 1237, 45));
+        Console.WriteLine(Boolean16(125));
+        Console.WriteLine(Boolean17(255));
+        Console.WriteLine(Boolean18(255, 89, 23));
+        Console.WriteLine(Boolean19(587, 124, 387));
+        Console.WriteLine(Boolean20(123));
     }
 
     static bool Boolean1(int a)
@@ -65,4 +75,59 @@ public class Boolean
     {
         return a % 2 != 0 ^ b % 2 != 0;
     }
+
+    static bool Boolean11(int a, int b)
+    {
+        return (a % 2 != 0 && b % 2 != 0) || (a % 2 == 0 && b % 2 == 0); 
+    }
+
+    static bool Boolean12(int a, int b, int c)
+    {
+        return a > 0 && b > 0 && c > 0;
+    }
+
+    static bool Boolean13(int a, int b, int c)
+    {
+        return a > 0 || b > 0 || c > 0;
+    }
+    
+    static bool Boolean14(int a, int b, int c)
+    {
+        return a > 0 ^ b > 0 ^ c > 0;
+    }
+    
+    static bool Boolean15(int a, int b, int c)
+    {
+        return (a > 0 && b > 0) ^ (a > 0 && c > 0) ^ (b > 0 && c > 0);
+    }
+    
+    static bool Boolean16(int a)
+    {
+        return a < 100 && a > 9 && a % 2 == 0;
+    }
+
+    static bool Boolean17(int a)
+    {
+        return a < 1000 && a > 99 && a % 2 != 0;
+    }
+    
+    static bool Boolean18(int a, int b, int c)
+    {
+        return (a == b) || (a == c) || (b == c);
+    }
+    
+    static bool Boolean19(int a, int b, int c)
+    {
+        return (a > 0 && b < 0) || (a < 0 && b > 0) || (a > 0 && c < 0) || (a < 0 && c > 0) || (b > 0 && c < 0) || (b < 0 && c > 0);
+    }
+
+    static bool Boolean20(int abc)
+    {
+        int a = abc / 100;
+        int b = abc / 10 % 10;
+        int c = abc % 10;
+        return a != b && a != c && b != c;
+    }
+    
+    
 }
