8cc915f5861726803019ae25cd61e6dee5ce542d
Zenisoft <60827259+TimurSl@users.noreply.github.com>
2023-01-17 12:36:04 +0200

    For 20 / 40

--- Tasks/For.cs
+++ Tasks/For.cs
@@ -24,6 +24,26 @@ public class For
         For9(14, 15);
         Console.WriteLine("For10: ");
         For10(17);
+        Console.WriteLine("For11: ");
+        For11(17);
+        Console.WriteLine("For12: ");
+        For12(17);
+        Console.WriteLine("For13: ");
+        For13(17);
+        Console.WriteLine("For14: ");
+        For14(17);
+        Console.WriteLine("For15: ");
+        For15(17, 12);
+        Console.WriteLine("For16: ");
+        For16(17, 12);
+        Console.WriteLine("For17: ");
+        For17(17, 12);
+        Console.WriteLine("For18: ");
+        For18(17, 12);
+        Console.WriteLine("For19: ");
+        For19(17);
+        Console.WriteLine("For20: ");
+        For20(17);
     }
 
     public static void For1(int n, int k)
@@ -119,5 +139,109 @@ public class For
         }
         Console.WriteLine(sum);
     }
-
+    public static void For11(int n)
+    {
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += (i * i) + ((i + 1) * (i + 1));
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For12(int n)
+    {
+        double sum = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sum *= (1 + 1.0 / i);
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For13(int n)
+    {
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += (1.0 / i) * Math.Pow(-1, i);
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For14(int n)
+    {
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += (2 * i - 1);
+            Console.WriteLine(sum);
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For15(double a, int n)
+    {
+        double sum = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sum *= a;
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For16(double a, int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(Math.Pow(a, i));
+        }
+    }
+    
+    public static void For17(double a, int n)
+    {
+        double sum = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += Math.Pow(a, i);
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For18(double a, int n)
+    {
+        double sum = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += Math.Pow(-1, i) * Math.Pow(a, i);
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For19(int n)
+    {
+        double sum = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sum *= i;
+        }
+        Console.WriteLine(sum);
+    }
+    
+    public static void For20(int n)
+    {
+        double sum = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += Factorial(i);
+        }
+        Console.WriteLine(sum);
+    }
+    
+    static int Factorial(int n)
+    {
+        if (n == 1) return 1;
+ 
+        return n * Factorial(n - 1);
+    }
 }
