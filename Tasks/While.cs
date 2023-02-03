0481ce7b0b25c64fc412865d230ed2bcde10dcdc
Zenisoft <60827259+TimurSl@users.noreply.github.com>
2023-02-03 13:00:16 +0200

    While ( 30 / 30 ) Total: 220

--- Tasks/While.cs
+++ Tasks/While.cs
@@ -4,27 +4,66 @@ public class While
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("While 1: ");
         While1(1, 3);
+        Console.WriteLine("While 2: ");
         While2(1, 3);
+        Console.WriteLine("While 3: ");
         While3(1, 3);
+        Console.WriteLine("While 4: ");
         While4(3);
+        Console.WriteLine("While 5: ");
         While5(4);
+        Console.WriteLine("While 6: ");
         While6(5);
+        Console.WriteLine("While 7: ");
         While7(6);
+        Console.WriteLine("While 8: ");
         While8(7);
+        Console.WriteLine("While 9: ");
         While9(8);
+        Console.WriteLine("While 10: ");
         While10(9);
+        Console.WriteLine("While 11: ");
         While11(10);
+        Console.WriteLine("While 12: ");
         While12(11);
+        Console.WriteLine("While 13: ");
         While13(12);
+        Console.WriteLine("While 14: ");
         While14(13);
+        Console.WriteLine("While 15: ");
         While15(14);
+        Console.WriteLine("While 16: ");
         While16(15);
+        Console.WriteLine("While 17: ");
         While17(16);
+        Console.WriteLine("While 18: ");
         While18(17);
+        Console.WriteLine("While 19: ");
         While19(18);
+        Console.WriteLine("While 20: ");
         While20(19);
-        
+        Console.WriteLine("While 21: ");
+        While21(35);
+        Console.WriteLine("While 22: ");
+        While22(12);
+        Console.WriteLine("While 23: ");
+        While23(4, 5);
+        Console.WriteLine("While 24: ");
+        While24(67);
+        Console.WriteLine("While 25: ");
+        While25(45);
+        Console.WriteLine("While 26: ");
+        While26(4);
+        Console.WriteLine("While 27: ");
+        While27(42);
+        Console.WriteLine("While 28: ");
+        While28(23);
+        Console.WriteLine("While 29: ");
+        While29(12);
+        Console.WriteLine("While 30: ");
+        While30(30, 31, 32);
     }
 
     public static void While1(int a, int b)
@@ -224,19 +263,20 @@ public class While
         Console.WriteLine($"K = {k}, S = {s}");
     }
 
-    public static void While16(int k)
+    public static void While16(int p)
     {
-        double s = 10.0;
-        double p = 0.0;
-
-        while (s < 200)
+        int k = 1;
+        double s = 10;
+        double sum = 10;
+        
+        while (sum <= 200)
         {
-            k = k++;
-            s = s * (1 + p / 100.0);
+            k++;
+            s = s + s * p / 100;
+            sum += s;
         }
-
-        Console.WriteLine("K  = " + k);
-        Console.WriteLine("S = " + Math.Round(s,3));
+        
+        Console.WriteLine($"K = {k}, S = {sum}");
     }
 
     public static void While17(int n)
@@ -296,5 +336,169 @@ public class While
         }
         Console.WriteLine(isTwoInNumber);
     }
+    
+    public static void While21(int n)
+    {
+        bool isOdd = false;
 
+        while (n > 0)
+        {
+            int reminder = n % 10;
+            if (reminder % 2 != 0)
+            {
+                isOdd = true;
+                break;
+            }
+
+            n /= 10;
+        }
+        Console.WriteLine(isOdd);
+    }
+    
+    public static void While22(int n)
+    {
+        bool isPrime = true;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        Console.WriteLine(isPrime);
+    }
+    
+    public static void While23(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        Console.WriteLine(a);
+    }
+    
+    public static void While24(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+        bool isFibonacci = false;
+
+        while (f3 < n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+            if (f3 == n)
+            {
+                isFibonacci = true;
+                break;
+            }
+        }
+        Console.WriteLine(isFibonacci);
+    }
+    
+    public static void While25(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+
+        while (f3 <= n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        Console.WriteLine(f3);
+    }
+    
+    public static void While26(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+
+        while (f3 < n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        Console.WriteLine($"Fk-1 = {f1}");
+        Console.WriteLine($"Fk+1 = {f3}");
+    }
+    
+    public static void While27(int n)
+    {
+        int f1 = 1;
+        int f2 = 1;
+        int f3 = 0;
+        int k = 2;
+
+        while (f3 < n)
+        {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+            k++;
+        }
+        Console.WriteLine(k);
+    }
+    
+    public static void While28(double e)
+    {
+        double a1 = 2;
+        double a2 = 2 + 1 / a1;
+        int k = 2;
+
+        while (Math.Abs(a2 - a1) >= e)
+        {
+            a1 = a2;
+            a2 = 2 + 1 / a1;
+            k++;
+        }
+        Console.WriteLine($"K = {k}");
+        Console.WriteLine($"A1 = {a1}");
+        Console.WriteLine($"A2 = {a2}");
+    }
+    
+    public static void While29(double e)
+    {
+        double a1 = 1;
+        double a2 = 2;
+        double a3 = (a1 + 2 * a2) / 3;
+        int k = 3;
+
+        while (Math.Abs(a3 - a2) >= e)
+        {
+            a1 = a2;
+            a2 = a3;
+            a3 = (a1 + 2 * a2) / 3;
+            k++;
+        }
+        Console.WriteLine($"K = {k}");
+        Console.WriteLine($"A1 = {a1}");
+        Console.WriteLine($"A2 = {a2}");
+        Console.WriteLine($"A3 = {a3}");
+    }
+    public static void While30(int a, int b, int c)
+    {
+        int count = 0;
+        int a1 = a;
+        int b1 = b;
+
+        while (a1 >= c && b1 >= c)
+        {
+            a1 -= c;
+            b1 -= c;
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+    
 }
